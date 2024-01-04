3dde1909cd6b9ae7c20554601619ce97653a2720
SANHOKIM <awrko@naver.com>
2024-01-04 15:10:30 +0900

    Output 태그 제거 , InputUi 컴포넌태태그 추가 HrefBtn 컴포넌트태그 추가

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-//
 import "./App.css";
-// npm i 추측 하기
 import "bootstrap/dist/css/bootstrap.css";
-// npm i 추측 하기
 import styled from "styled-components";
-// src 의 svg 파일 삽입
 import arrowbtnimg from "./icon/arrowbtnimg.svg";
-// Commonui 컴포넌트 안의 작은 컴포넌트 TitleCommon
-import { TitleCommon } from "./Commonui";
-
-import { ButtonUi, Abtn, Output } from "./Commonui";
+import { TitleCommon, InputUi, ButtonUi, Hrefbtn, Abtn } from "./Commonui";
 
 function App() {
   return (
-    <div className="App bg-primary p-2 d-flex">
-      <TitleCommon>필수정보</TitleCommon>
-      <TitleCommon>다른섹션</TitleCommon>
-      <ButtonUi>
-        Go toto Website
-        <img src={arrowbtnimg} alt="" />
-      </ButtonUi>
-      <Output href="http://www.naver.com" target="_blank">
-        Output태그
-      </Output>
-      <Abtn href="http://www.google.com" className="ddd" target="_blank">
-        내용ddd
-      </Abtn>
+    <div className="App bg-light p-5">
+      <TitleCommon>필수 정보</TitleCommon>
+
+      <form action="">
+        <div className="row">
+          <InputUi
+            className=""
+            inputinfo={{
+              nm: "company",
+              labeltext: "회사이름",
+              alerttext: "회사명을 입력해주세요.",
+            }}
+          >
+            회사이름(필수)
+          </InputUi>
+          <ButtonUi>
+            Go toto Website
+            <img src={arrowbtnimg} alt="" />
+          </ButtonUi>
+          <Hrefbtn>아하</Hrefbtn>
+          <Abtn>dddd</Abtn>
+        </div>
+      </form>
     </div>
   );
 }
-
 export default App;
--- src/Commonui.js
+++ src/Commonui.js
@@ -2,14 +2,37 @@ import styled from "styled-components";
 import "@fontsource/noto-sans-kr";
 import arrowbtnimg from "./icon/arrowbtnimg.svg";
 
+// 타이틀
 export const TitleCommon = styled.h2`
   font-family: "Noto Sans KR";
   font-weight: 500;
   font-size: 1.8rem;
   text-align: center;
 `;
+// 폼
 
-export const ButtonUi = styled("button")`
+export const InputUi = (props) => {
+  return (
+    <div className={props.className && props.className}>
+      <div>
+        <input
+          type="text"
+          name={props.inputinfo.nm}
+          placeholder={props.children}
+        />
+        <label htmlFor="">
+          {props.inputinfo.labeltext}
+          <i></i>
+        </label>
+      </div>
+      <span>{props.inputinfo.alerttext}</span>
+    </div>
+  );
+};
+
+// 버튼
+
+export const ButtonUi = styled.button`
   width: 250px;
   line-height: 3rem;
   font-size: 1.5rem;
@@ -17,11 +40,22 @@ export const ButtonUi = styled("button")`
   background-color: white;
 `;
 
+export const Hrefbtn = styled.a`
+  display: inline-block;
+  width: 250px;
+  line-height: 3.1rem;
+  font-size: 1.5rem;
+  border: 1px solid #000;
+  text-decoration: none;
+  color: black;
+`;
+
 export const Abtn = (props) => {
   return (
     <a
       href={props.href}
       className={props.className && props.className}
+      //스타일컴포넌트를 엄마컴포넌트가 아닐때를 대비
       target={props.target && props.target}
     >
       {props.children}
@@ -30,12 +64,6 @@ export const Abtn = (props) => {
   );
 };
 
-export const Output = styled(Abtn)`
-  display: inline-block;
-  width: 250px;
-  line-height: 3.1rem;
-  font-size: 1.5rem;
-  border: 1px solid #000;
-  text-decoration: none;
-  color: red;
-`;
+// 목록
+
+// 갤러리
